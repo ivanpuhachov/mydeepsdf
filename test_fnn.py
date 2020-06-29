deba7425d547c256e489e52f7f3ede17ca9cdd7f
ivan <ivan.puhachov@gmail.com>
2020-06-29 14:14:36 -0400

    test_fnn with training plots and final validation

--- test_fnn.py
+++ test_fnn.py
@@ -3,6 +3,7 @@ import numpy as np
 from torch.utils.data import DataLoader, TensorDataset, random_split
 import torch.nn as nn
 from models import SingleShapeSDF, deepsdfloss
+import matplotlib.pyplot as plt
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -11,13 +12,23 @@ with open('data/chair.npy', 'rb') as f:
     labels = torch.from_numpy(np.load(f))
 
 dataset = TensorDataset(features, labels)
-trainset, valset, testset = random_split(dataset, [200000, 10000, 40000])
+trainset, valset, testset = random_split(dataset, [250000, 50000, 200000])
 
 train_loader = DataLoader(
     trainset,
     shuffle=True,
     batch_size=5000)
 # TODO: paper uses batchsize = 16k, why?
+
+validation_loader = DataLoader(
+    valset,
+    shuffle=False,
+    batch_size=5000
+)
+
+test_loader = DataLoader(
+    testset, shuffle=False, batch_size=5000
+)
 
 
 def test_overfitting(mymodel, dataloader, lossfunction, learning_rate=1e-4, n_iters=30):
@@ -37,13 +48,17 @@ def test_overfitting(mymodel, dataloader, lossfunction, learning_rate=1e-4, n_it
         print(loss.item())
 
 
-def test_training(mymodel, dataloader, lossfunction, learning_rate=1e-4, n_epochs=10):
+def test_training(mymodel, dataloader, valloader, lossfunction, learning_rate=1e-4, n_epochs=10):
     print(mymodel)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     optimizer = torch.optim.Adam(mymodel.parameters(), lr=learning_rate)
+    train_history = []
+    validation_history = []
     for epoch in range(n_epochs):
+        mymodel.train(True)
         print(f"\nEpoch {epoch}")
         running_loss = 0
+        total_loss = 0
         for i, data in enumerate(dataloader, 0):
             x, y = data[0].to(device), data[1].unsqueeze(1).to(device)
             y_pred = mymodel(x)
@@ -52,10 +67,23 @@ def test_training(mymodel, dataloader, lossfunction, learning_rate=1e-4, n_epoch
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
-            if i % 10 == 9:
+            total_loss += loss.item()
+            if i % 20 == 9:
                 print(i, running_loss)
                 running_loss = 0
 
+        train_history.append(total_loss/20) # /20 to normalize and compare with validation set
+        mymodel.train(False)
+        total_loss = 0
+        with torch.no_grad():
+            for i, data in enumerate(valloader):
+                x, y = data[0].to(device), data[1].unsqueeze(1).to(device)
+                y_pred = mymodel.forward(x)
+                loss = lossfunction(y_pred, y)
+                total_loss += loss.item()
+        validation_history.append(total_loss)
+    plt.plot(range(n_epochs), train_history, label='train')
+    plt.plot(range(n_epochs), validation_history, label='val')
 
 
 model = SingleShapeSDF([512, 512]).to(device)
@@ -64,9 +92,24 @@ model = SingleShapeSDF([512, 512]).to(device)
 
 # loss_fn = torch.nn.MSELoss(reduction='sum')
 loss_fn = deepsdfloss
-
+n_epochs = 5
 # test_overfitting(model, train_loader, loss_fn)
-test_training(model, train_loader, loss_fn)
+test_training(model, train_loader, validation_loader, loss_fn, n_epochs=n_epochs)
 
-# TODO: validation
+model.train(False)
+test_loss = 0
+with torch.no_grad():
+    for i, data in enumerate(test_loader):
+        x, y = data[0].to(device), data[1].unsqueeze(1).to(device)
+        y_pred = model.forward(x)
+        loss = loss_fn(y_pred, y)
+        test_loss += loss.item()
+print(f"TEST LOSS: {test_loss/4}")
+plt.axhline(y=test_loss/4, xmin=0, xmax=n_epochs-1, color='red', label='final test')
+plt.legend()
+plt.title("Loss")
+plt.show()
+
+# TODO: validation with another metric (not deepsdf loss)
+# TODO: what metric do they use in the paper?
 # TODO: visualization
